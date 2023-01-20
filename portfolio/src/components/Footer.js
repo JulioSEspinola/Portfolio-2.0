99181b970c7e5fce31f0b140c5bafc949190fb64
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2023-01-19 16:41:21 -0800

    Footer update

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GithubIcont from '@mui/icons-material/GitHub';
+import { IconButton } from '@mui/material';
 import '../styles/Footer.css';
 
 export default function Footer() {
   return (
     <div className='footer'>
       <div className='socialMedia'>
-        <GithubIcont/>
+      <IconButton href="https://github.com/JulioSEspinola" target="_blank">
+          <GithubIcont />
+      </IconButton>
+
+      <IconButton href="https://www.linkedin.com/in/julio-steven-espinola-rodas-734902190/" target="_blank">
         <LinkedInIcon/>
+      </IconButton>
       </div>
       <p>&copy; 2023 Julio Espinola.com </p>
     </div>
