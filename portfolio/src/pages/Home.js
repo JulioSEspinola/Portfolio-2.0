dbaaca5582ed70d453c9d9059aa9c881d28f16c6
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2023-09-07 23:34:23 -0700

    Cleaned extra spaces in this file

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -3,36 +3,23 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GithubIcon from '@mui/icons-material/GitHub';
 import { IconButton } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
-// import Link from '@mui/icons-material/Link';
 import '../styles/Home.css'
-
 
 function home() {
   return (
-
     <div className='home'>
-   
-     
       <div className='about'>
-
         <h2 >Hi!, I'm Julio Espinola</h2>
-
         <div className='prompt'>
-      
           <IconButton href="https://github.com/JulioSEspinola" target="_blank">
             <GithubIcon />
           </IconButton>
-
           <IconButton href="https://www.linkedin.com/in/julio-steven-espinola-rodas-734902190/" target="_blank">
             <LinkedInIcon/>
-
           </IconButton>
-
           <IconButton href="mailto: espinolaj192@gmail.com" target="_blank">
              <EmailIcon/>
-            
           </IconButton>
-       
         </div>
       </div>
       <div className='skills'>
@@ -43,7 +30,6 @@ function home() {
             one of my goals is to inspire those with similar backgrounds to pursue more involvement in the tech industry. I am an individual how is eager to learn
             and make a change.
         </p>
-
         <h1 className='skills-h'>Skills</h1>
         <ol className='list'>
           <li className='item'>
@@ -52,12 +38,10 @@ function home() {
               HTML | CSS | BootStap | JavaScript | React
             </span>
           </li>
-
           <li className='item'>
             <h2> Back End</h2>
             <span>Java | Python | MySQL</span>
           </li>
-
           <li className='item'>
             <h2> Softwares & Tools</h2>
             <span>Git | GitHub | VS Code | IntelliJ | Heroku</span>
@@ -65,8 +49,6 @@ function home() {
         </ol>
       </div>
     </div>
-    
   )
 }
-
 export default home
