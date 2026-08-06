76e75daffca8aaf7fa064eafdd646a4bbb164896
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2026-08-06 16:48:30 -0500

    fix: removed old projects from the list of proejects

--- portfolio/src/helpers/ProjectList.js
+++ portfolio/src/helpers/ProjectList.js
@@ -1,5 +1,5 @@
-import Project1 from '../assets/crypto.png';
-import Project2 from '../assets/uBelong.png';
+// import Project1 from '../assets/crypto.png';
+// import Project2 from '../assets/uBelong.png';
 import Project3 from '../assets/ReceiptReader.png'
 import Project4 from '../assets/websio_bckgrnd.png'
 import Project5 from '../assets/Eltrifyo.png';
@@ -8,34 +8,20 @@ import Project5 from '../assets/Eltrifyo.png';
 export const ProjectList = [
     {
         id:1,
-        name: "Crypto Currency Ledger",
-        image: Project1,
-        description: "The Crypto Currency Ledger is a collaborative group project for my CS380 Software Engineering class. It is used as centralized crypto currency wallet. This app allows you to see your top three coins, add notes, and provides a modern dashboard UI that displays relevant information for everyday traders.",
-        link: "https://github.com/JulioSEspinola/Cryptocurrency-Ledger"
-    },
-    {
-        id:2,
-        name: "uBeLong",
-        image: Project2,
-        description: "uBelong is a web application developed during my internship at ThoughtWorks with the purpose to keep historically marginalized communites aware of their rights and prevent family separation. uBelong provides features that are community driven, has an accessible design, a central location of resources and legal support.",
-        link:"https://github.com/JulioSEspinola/tw-na-internship-project"
-    },
-    {
-        id:3,
         name: "Receipt Reader",
         image: Project3,
         description: "The Reciept Reader project is a project for the CS 481 Capstone class at Central Washington University. The purpose of this project is to allow users to keep track of their expenses by gathering important information from the reciepts of the stores that they shop from.",
         link:"https://github.com/Reciept-reader/reciept-reader"
     },
     {
-        id:4,
+        id:2,
         name: "Websio",
         image: Project4,
         description: "A freelance software development company specializing in creating and maintaining websites using content management systems (CMS) for non-profits. Expertise includes developing and customizing themes and plugins to meet specific client needs, ensuring seamless functionality. Security is a priority, with SSL certificates and firewalls implemented to safeguard websites, along with ongoing maintenance tasks such as updates, backups, and bug fixes. Focused on delivering tailored, high-quality solutions, the goal is to help non-profits optimize their online presence through secure and efficient web development.",
         link:"https://juliosespinola.github.io/Websio/"
     },
     {
-        id:5,
+        id:3,
         name: "Electrifyo",
         image: Project5,
         description: "This application, Electrifyo, is a web-based tool designed to analyze electricity usage and carbon footprint data for specific locations. Users can input their address, city, state, and zip code to retrieve detailed monthly electricity costs and annual carbon footprint estimates. The app visualizes the data using interactive charts and provides a user-friendly interface for exploring energy consumption insights. It also includes features to reset the displayed data and dynamically update the content based on user input.",
